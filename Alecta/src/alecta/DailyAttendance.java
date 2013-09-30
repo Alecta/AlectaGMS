d521985ce9c51d664cadde96556bfcb622a0e010
Bhagya Gayathri <bhagyah4@gmail.com>
2013-09-30 14:39:35 +0530

    Attendance record

--- Alecta/src/alecta/DailyAttendance.java
+++ Alecta/src/alecta/DailyAttendance.java
@@ -13,6 +13,22 @@ public class DailyAttendance {
     private boolean leave;
     private boolean present;
 
+    public void setArrival(Time arrival) {
+        this.arrival = arrival;
+    }
+
+    public void setDeparture(Time departure) {
+        this.departure = departure;
+    }
+
+    public void setLeave(boolean leave) {
+        this.leave = leave;
+    }
+
+    public void setPresent(boolean present) {
+        this.present = present;
+    }
+
     public boolean isLeave() {
         return leave;
     }
